d111d26dec55594a5d9cf137cbf9fd96ad15816f
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-10-04 11:25:36 +0500

    Добавьте файлы проекта.

---
+++ ex40/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Renderer renderer = new Renderer();
+            Player player = new Player(50, 16);
+
+            renderer.DrawPlayer(player.X, player.Y);
+        }
+    }
+
+    class Renderer
+    {
+        public void DrawPlayer(int x, int y, char character = '@')
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(character);
+            Console.ReadKey(true);
+        }
+    }
+
+    class Player
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
